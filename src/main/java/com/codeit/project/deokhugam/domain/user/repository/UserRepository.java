950c74f25106851836cf518eec9db8173b7ca0af
yuhoyuho <leeyuho123@naver.com>
2025-10-21 17:36:28 +0900

    [feat] UserRepository 구현

---
+++ src/main/java/com/codeit/project/deokhugam/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codeit.project.deokhugam.domain.user.repository;
+
+import com.codeit.project.deokhugam.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+
+}
---
+++ src/main/java/com/codeit/project/deokhugam/domain/user/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.codeit.project.deokhugam.domain.user.repository.impl;
+
+import com.codeit.project.deokhugam.domain.user.repository.UserRepositoryCustom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepositoryCustom {
+
+}
