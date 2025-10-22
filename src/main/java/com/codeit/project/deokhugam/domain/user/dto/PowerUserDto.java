af5b8e13ab8b91e57bc61ac0ea83a9485affc2cd
yuhoyuho <leeyuho123@naver.com>
2025-10-22 18:20:48 +0900

    fix : period 타입 수정

--- src/main/java/com/codeit/project/deokhugam/domain/user/dto/PowerUserDto.java
+++ src/main/java/com/codeit/project/deokhugam/domain/user/dto/PowerUserDto.java
@@ -1,11 +1,11 @@
 package com.codeit.project.deokhugam.domain.user.dto;
 
-import com.codeit.project.deokhugam.domain.user.enums.Period;
+import com.codeit.project.deokhugam.domain.rank.entity.RankType;
 
 public record PowerUserDto(
     String userId,
     String nickname,
-    Period period,
+    RankType period,
     String createdAt,
     Integer rank,
     Integer score,
