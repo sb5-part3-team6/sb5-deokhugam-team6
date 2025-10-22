81761d07dfc4e1abce17b678c990be096de05f11
Soyeon <isylsy166@gmail.com>
2025-10-22 11:37:49 +0900

    [feat] Review: 커스텀 Repository 생성 및 구현

---
+++ src/main/java/com/codeit/project/deokhugam/domain/review/repository/ReviewRepositoryCustom.java
@@ -0,0 +1,61 @@
+package com.codeit.project.deokhugam.domain.review.repository;
+
+import com.codeit.project.deokhugam.domain.review.dto.ReviewQueryParams;
+import com.codeit.project.deokhugam.domain.review.entity.QReview;
+import com.codeit.project.deokhugam.domain.review.entity.Review;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ReviewRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public List<Review> list(ReviewQueryParams params) {
+
+        QReview review = QReview.review;
+        BooleanBuilder where = new BooleanBuilder();
+
+        if(params.userId() != null && !params.userId().toString().trim().isEmpty())
+            where.and(review.user.id.eq(params.userId()));
+
+        if(params.bookId() != null && !params.bookId().toString().trim().isEmpty())
+            where.and(review.book.id.eq(params.bookId()));
+
+        if(params.keyword() != null && !params.keyword().trim().isEmpty()){
+            where.and(review.user.nickname.containsIgnoreCase(params.keyword()));
+            where.and(review.content.containsIgnoreCase(params.keyword()));
+        }
+
+        OrderSpecifier<?> orderSpecifier = makeOrderSpecifier(params.direction(), params.orderBy());
+
+        return queryFactory.selectFrom(review)
+                .where(where)
+                .orderBy(orderSpecifier)
+                .limit(params.limit() + 1)
+                .fetch();
+    }
+
+
+    private OrderSpecifier<?> makeOrderSpecifier(String direction, String orderBy) {
+
+        QReview review = QReview.review;
+
+        Order order = "desc".equalsIgnoreCase(direction) ? Order.DESC : Order.ASC;
+        OrderSpecifier<?> orderSpecifier;
+
+        switch (orderBy) {
+            case "rating" -> orderSpecifier = new OrderSpecifier<>(order, review.rating);
+            default -> orderSpecifier = new OrderSpecifier<>(order, review.createdAt);
+        }
+
+        return orderSpecifier;
+    }
+}
