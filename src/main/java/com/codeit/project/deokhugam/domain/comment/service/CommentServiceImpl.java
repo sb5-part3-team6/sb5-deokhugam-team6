fa5dc7826f1c26a5238c18fdd840ec925818b714
KimDongGyu <redmatoda@naver.com>
2025-10-23 11:29:37 +0900

    feat : 댓글 serviceImpl 필요없는 import 제거

--- src/main/java/com/codeit/project/deokhugam/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/codeit/project/deokhugam/domain/comment/service/CommentServiceImpl.java
@@ -11,7 +11,6 @@ import com.codeit.project.deokhugam.domain.review.entity.Review;
 import com.codeit.project.deokhugam.domain.review.entity.repository.ReviewRepository;
 import com.codeit.project.deokhugam.domain.user.entity.User;
 import com.codeit.project.deokhugam.domain.user.entity.repository.UserRepository;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
