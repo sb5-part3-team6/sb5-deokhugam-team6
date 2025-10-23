e39544bb6eecaa6334609fab1cbba8831f1caa63
yuhoyuho <leeyuho123@naver.com>
2025-10-23 17:20:23 +0900

    feat : 커스텀 예외 추가

--- src/main/java/com/codeit/project/deokhugam/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/codeit/project/deokhugam/domain/user/exception/UserErrorCode.java
@@ -9,12 +9,12 @@ public enum UserErrorCode implements ErrorCode {
 
   INVALID_INPUT_VALUE("유효하지 않은 입력입니다.", HttpStatus.BAD_REQUEST),
 
-  EMAIL_DUPLICATION("중복된 이메일입니다.", HttpStatus.CONFLICT),
-  NICKNAME_DUPLICATION("중복된 닉네임입니다.", HttpStatus.CONFLICT),
+  EMAIL_DUPLICATION("이미 존재하는 이메일입니다.", HttpStatus.CONFLICT),
+  NICKNAME_DUPLICATION("이미 존재하는 닉네임입니다.", HttpStatus.CONFLICT),
 
   USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
-  LOGIN_INPUT_INVALID("이메일 또는 비밀번호를 확인해주세요", HttpStatus.UNAUTHORIZED),
+  LOGIN_INPUT_INVALID("이메일 또는 비밀번호를 확인해주세요.", HttpStatus.UNAUTHORIZED),
 
   USER_ALREADY_DELETED("이미 삭제된 사용자입니다.", HttpStatus.CONFLICT),
   DELETE_NOT_ALLOWED("Soft Delete가 수행되지 않았습니다.", HttpStatus.CONFLICT);
