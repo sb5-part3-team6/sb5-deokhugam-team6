c78255d938e4d852d8c212c253a5f803f59ce72e
이유호 <leeyuho123@naver.com>
2025-10-23 18:22:03 +0900

    [feat] 사용자 API 커스텀 예외 적용 및 단위 테스트 추가 (#35)
    * feat : ErrorCode 작성
    
    * feat : 커스텀 예외 추가
    
    * fix : 커스텀 예외로 수정
    
    * test : TC 추가 작성 및 커스텀 예외로 수정

--- src/main/java/com/codeit/project/deokhugam/domain/user/exception/UserExceptionHandler.java
+++ src/main/java/com/codeit/project/deokhugam/domain/user/exception/UserExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.codeit.project.deokhugam.domain.user.exception;
 
 import com.codeit.project.deokhugam.domain.user.controller.UserController;
-import java.util.NoSuchElementException;
+import com.codeit.project.deokhugam.global.common.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,27 +10,82 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(assignableTypes = UserController.class)
+@Slf4j
 public class UserExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request");
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    log.error("회원가입 에러 발생 : message = {}", exception.getMessage(), exception);
+
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+
+    return ResponseEntity.status(status)
+        .body(response);
   }
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request");
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleIllegalArgumentException(UserNotFoundException exception) {
+    log.error("사용자 찾을 수 없음 : code = {}, message = {}", exception.getErrorCode(), exception.getMessage());
+
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
   }
 
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
+  @ExceptionHandler(UserAlreadyDeletedException.class)
+  public ResponseEntity<ErrorResponse> handleUserAlreadyDeletedException(UserAlreadyDeletedException exception) {
+    log.error("사용자가 이미 삭제됨 : code = {}, message = {}", exception.getErrorCode(), exception.getMessage());
+
+    HttpStatus status = HttpStatus.CONFLICT;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
+  }
+
+  @ExceptionHandler(NicknameDuplicationException.class)
+  public ResponseEntity<ErrorResponse> handleNicknameDuplicationException(NicknameDuplicationException exception) {
+    log.error("중복된 닉네임 : code = {}, message = {}", exception.getErrorCode(), exception.getMessage());
+
+    HttpStatus status = HttpStatus.CONFLICT;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
+  }
+
+  @ExceptionHandler(EmailDuplicationException.class)
+  public ResponseEntity<ErrorResponse> handleEmailDuplicationException(EmailDuplicationException exception) {
+    log.error("중복된 이메일 : code = {}, message = {}", exception.getErrorCode(), exception.getMessage());
+
+    HttpStatus status = HttpStatus.CONFLICT;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
+  }
+
+  @ExceptionHandler(DeleteNotAllowedException.class)
+  public ResponseEntity<ErrorResponse> handleDeleteNotAllowedException(DeleteNotAllowedException exception) {
+    log.error("Soft Delete 되지 않은 사용자 : code = {}, message = {}", exception.getErrorCode(), exception.getMessage() );
+
+    HttpStatus status = HttpStatus.CONFLICT;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
+  }
+
+  @ExceptionHandler(LoginInputInvalidException.class)
+  public ResponseEntity<ErrorResponse> handleNoSuchElementException(LoginInputInvalidException exception) {
+    log.error("로그인 에러 발생 : code = {}, message = {}", exception.getMessage(), exception);
+
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    ErrorResponse response = new ErrorResponse(exception, status.value());
+    return ResponseEntity.status(status)
+        .body(response);
   }
 
   @ExceptionHandler(IllegalStateException.class)
   public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict");
   }
-
-
 }
