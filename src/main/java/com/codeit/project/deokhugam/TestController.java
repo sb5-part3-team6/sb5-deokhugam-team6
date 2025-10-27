52c635b2590c536b011c61e2227f6774cd92d0a2
Soyeon <isylsy166@gmail.com>
2025-10-27 16:43:53 +0900

    [feat] api 테스트용 컨트롤러 (임시)

--- src/main/java/com/codeit/project/deokhugam/TestController.java
+++ src/main/java/com/codeit/project/deokhugam/TestController.java
@@ -1,5 +1,8 @@
 package com.codeit.project.deokhugam;
 
+import com.codeit.project.deokhugam.domain.book.dto.BookDto;
+import com.codeit.project.deokhugam.openapi.api.NaverBookApiClient;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -7,8 +10,11 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,6 +24,7 @@ public class TestController {
 
     private final JobLauncher jobLauncher;
     private final Job dailyReviewStatJob;
+    private final NaverBookApiClient naverBookApiClient;
 
     @GetMapping("/force-fetch")
     public String forceFetchNews() {
@@ -45,5 +52,10 @@ public class TestController {
         } catch (Exception e) {
             log.error("{} batch job 실패", name, e);
         }
+    }
+
+    @GetMapping("/naver/book")
+    public void getBook(@PathParam("isbn") String isbn) {
+        naverBookApiClient.fetchBooks(isbn);
     }
 }
