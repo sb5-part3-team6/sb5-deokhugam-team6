e17c94180767933b188f131225f56e996327a689
Soyeon <isylsy166@gmail.com>
2025-10-27 13:12:35 +0900

    [hotfix] webClient 설정파일 추가 및 build.gradle 의존성 추가 (#75)

---
+++ src/main/java/com/codeit/project/deokhugam/global/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.codeit.project.deokhugam.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public WebClient naverWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://openapi.naver.com")
+                .build();
+    }
+}
