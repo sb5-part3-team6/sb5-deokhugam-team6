4a2443ce6bf097d90b3a20a322fb8b0633083c1c
Moona <moonapark1126@gmail.com>
2025-10-28 14:41:36 +0900

    s3연동 -cdn

---
+++ src/main/java/com/codeit/project/deokhugam/domain/book/config/S3Config.java
@@ -0,0 +1,35 @@
+package com.codeit.project.deokhugam.domain.book.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@Profile("prod")
+public class S3Config {
+
+  @Value("${aws.accessKeyId}")
+  private String accessKeyId;
+
+  @Value("${aws.secretKey}")
+  private String secretKey;
+
+  @Value("${aws.region}")
+  private String region;
+
+  @Bean
+  public S3Client s3Client() {
+    AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKeyId, secretKey);
+
+    return S3Client.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+        .build();
+  }
+
+}
---
+++ src/main/java/com/codeit/project/deokhugam/domain/book/config/impl/FileConfigProd.java
@@ -0,0 +1,42 @@
+package com.codeit.project.deokhugam.domain.book.config.impl;
+
+import com.codeit.project.deokhugam.domain.book.config.FileConfig;
+import jakarta.annotation.PostConstruct;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class FileConfigProd implements FileConfig {
+
+  @Value("${file.upload-dir}")
+  private String uploadDir;
+
+  @PostConstruct
+  public void init() {
+    try {
+      Files.createDirectories(Path.of(uploadDir));
+    } catch (Exception e) {
+      throw new IllegalStateException("업로드 디렉토리 생성 실패 : " + uploadDir, e);
+    }
+  }
+
+  @Override
+  public File getThumbnailUploadDirFile() {
+    try {
+      Path p = Paths.get(uploadDir, "thumbnails");
+      Files.createDirectories(p);
+      return p.toFile();
+    } catch (Exception e) {
+      throw new IllegalStateException("아바타 디렉토리 생성 실패", e);
+    }
+  }
+}
+
+
+
